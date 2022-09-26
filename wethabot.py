8939abe9a682efc6b75be3635b2e6ea9a12eb387
Victor Balaniouk <balaniouk@gmail.com>
2022-09-26 22:51:13 +0200

    First upload

--- wethabot.py
+++ wethabot.py
@@ -1,1 +1,14 @@
-print('The weather is very good')
+import os
+import telebot
+
+#That part still doesn't work (need to find some solution)
+API_KEY = os.getenv('API_KEY')
+bot = telebot.TeleBot(API_KEY)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, '<b> Where are you from? </b>', parse_mode='html')
+
+bot.polling(none_stop=True)
+
+
