8d6b278d5070f6b97a50ad7f8c9be132fe4ae46c
Victor Balaniouk <balaniouk@gmail.com>
2022-09-28 08:06:03 +0200

    Solving key problem

--- wethabot.py
+++ wethabot.py
@@ -1,13 +1,22 @@
 import os
 import telebot
+from dotenv import load_dotenv
+load_dotenv()
 
 #That part still doesn't work (need to find some solution)
-API_KEY = os.getenv('API_KEY')
-bot = telebot.TeleBot(API_KEY)
+
+API_KEY_TG = os.getenv('API_KEY_TG')
+bot = telebot.TeleBot(API_KEY_TG)
+
 
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, '<b> Where are you from? </b>', parse_mode='html')
+
+# @bot.message_handler()
+# def get_user_text(message):
+
+
 
 bot.polling(none_stop=True)
 
